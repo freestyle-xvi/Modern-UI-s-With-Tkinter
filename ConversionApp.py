2271bca4145d654b8503e47dce38d96291e05425
Muhammad Hasan <m.hasanalware@gmail.com>
2024-03-11 21:49:41 +0200

    sizing of window and adding a label component

--- ConversionApp.py
+++ ConversionApp.py
@@ -4,6 +4,11 @@ from tkinter import ttk
 #creation of main window
 window = tk.Tk()
 window.title('Conversion App')
+window.geometry('300x300')
+
+#adding components
+HeadingLabel = ttk.Label(master = window, text='Miles to kilometers', font = 'Helvetica 24')
+HeadingLabel.pack()
 
 #displaying of window
 window.mainloop()
