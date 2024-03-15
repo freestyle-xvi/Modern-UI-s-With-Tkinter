0d3dfdfb9072adefd6f9d1f88dc82b2935bb9080
Muhammad Hasan <m.hasanalware@gmail.com>
2024-03-15 21:42:00 +0200

    Input & Output components added

--- ConversionApp.py
+++ ConversionApp.py
@@ -1,14 +1,34 @@
 import tkinter as tk
 from tkinter import ttk
 
+def convert():
+    print(entry.get())
+
+
+
 #creation of main window
 window = tk.Tk()
 window.title('Conversion App')
-window.geometry('300x300')
+window.geometry('800x500')
 
-#adding components
+#Title and text components
 HeadingLabel = ttk.Label(master = window, text='Miles to kilometers', font = 'Helvetica 24')
 HeadingLabel.pack()
+
+#Input components
+input_box = ttk.Frame(master=window)
+entryInt = tk.IntVar()
+entry = ttk.Entry(master=input_box, textvariable= entryInt)
+button = ttk.Button(master=input_box, text='Convert', command=convert)
+entry.pack(side='left', padx=10)
+button.pack()
+input_box.pack(pady=10)
+
+#Output Components
+output_label = ttk.Label(master=window, text='Output', font='Helvetica 24')
+output_label.pack(pady= 10)
+
+
 
 #displaying of window
 window.mainloop()
